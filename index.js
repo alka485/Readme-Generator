e98f133672548d85e12889649f1e7e0c00f42b65
Alka sah <alkasah485@gmail.com>
2022-07-30 08:35:59 -0700

    changes saved

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log("Hello");
 // TODO: Include packages needed for this application
  const inquirer = require('inquirer');
 
@@ -5,13 +6,29 @@
    const fs = require('fs');
 
 //Generate HTML files
-   const generateMd = require("./utils/generateMarkdown");
+   const generatePage = require("./utils/generateMarkdown");
+   
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+    {
+    type : 'input',
+    name : 'github_name',
+    message : 'What is your Github username?'
+    }
+];
+
+console.log(questions);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName ,data,(err) => {
+     err ? console.error(err) : console.log('submitted');
+    });
+}
+
+//const writeFileAsync = util.promisify(writeToFile);
 
 // TODO: Create a function to initialize app
 function init() {}
