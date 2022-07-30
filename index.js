f8519564e7357fc3b40d9284ca58c38c8c966c36
Alka sah <alkasah485@gmail.com>
2022-07-30 08:56:10 -0700

    added questions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("Hello");
+//console.log("Hello");
 // TODO: Include packages needed for this application
  const inquirer = require('inquirer');
 
@@ -7,19 +7,60 @@ console.log("Hello");
 
 //Generate HTML files
    const generatePage = require("./utils/generateMarkdown");
+const Choices = require('inquirer/lib/objects/choices');
    
 
 // TODO: Create an array of questions for user input
 const questions = [
     
     {
+        //1st question
     type : 'input',
     name : 'github_name',
     message : 'What is your Github username?'
-    }
+    },
+
+    //2nd question
+
+    {
+    type : 'input',
+    name : 'email_address',
+    message : 'What is your email address?'
+    },
+    
+    // 3rd question
+    {
+        type : 'input',
+        name : 'project_name',
+        message : 'What is your project name?'
+        },
+
+    //4th question   
+    {
+        type : 'input',
+        name : 'descr',
+        message : 'Please write description of project?'
+        },
+
+    // 5th question
+    {
+        type : 'input',
+        name : 'license',
+        message : 'What kind of license should your project have?',
+        choices : ['MIT','Apache','GPL'],
+        },
+
+    //6th question   
+    {
+        type : 'input',
+        name : 'descr',
+        message : 'What command should be run to run tests?'
+        }
+
+ 
 ];
 
-console.log(questions);
+//console.log(questions);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -31,7 +72,10 @@ function writeToFile(fileName, data) {
 //const writeFileAsync = util.promisify(writeToFile);
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions);
+
+}
 
 // Function call to initialize app
 init();
